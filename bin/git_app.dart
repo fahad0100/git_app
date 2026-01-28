17ce784ecdd175589a475e227edc66d63e68371e
fahad0100 <fahad.alazmi1994@gmail.com>
2026-01-28 10:40:04 +0300

    fix:code|20|20|24:fahad

---
+++ bin/git_app.dart
@@ -0,0 +1,5 @@
+import 'package:git_app/git_app.dart' as git_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${git_app.calculate()}!');
+}
---
+++ test/git_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:git_app/git_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
